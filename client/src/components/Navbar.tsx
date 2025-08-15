c8ef947e6873f425bac55e160d073e9f5df4af2c
Pavan Rayabarapu <pavanrayabarapu@Pavans-MacBook-Air.local>
2025-08-15 18:53:53 +0530

    Router setup

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+      <nav className="bg-gray-800 text-white px-6 py-4">
+        <h1 className="text-xl font-bold">LearnSphere LMS</h1>
+      </nav>
+    )
+  }
---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+    return(
+            <>
+                <div>Welcome to LearnSphere Dashboard</div>
+                <div>Signup</div>
+            </>
+        )}
